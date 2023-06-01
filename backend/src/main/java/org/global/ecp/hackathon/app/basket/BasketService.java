d45316bb06460a7be4141a2fa62b8ecadc3bca4e
Jyothyraj.Sobhana <jyothyraj.sobhana@global.com>
2023-06-01 10:43:59 +0100

    Removed product from basket in back end

--- backend/src/main/java/org/global/ecp/hackathon/app/basket/BasketService.java
+++ backend/src/main/java/org/global/ecp/hackathon/app/basket/BasketService.java
@@ -28,7 +28,7 @@ public class BasketService {
 
         BasketProduct basketProduct = basketProductService.createBasketProduct(getBasket(), productId);
         basketProduct.increaseQuantity();
-        return basketRepository.add(basketProduct);;
+        return basketRepository.add(basketProduct);
     }
 
     // TODO - Task 5: remove basketProduct from the basket using the basketRepository
@@ -36,7 +36,7 @@ public class BasketService {
 
         BasketProduct basketProduct = basketProductService.createBasketProduct(getBasket(), productId);
         basketProduct.decreaseQuantity();
-        return null;
+        return basketRepository.remove(basketProduct);
     }
 
     // TODO - Task 8: implement checkout method
