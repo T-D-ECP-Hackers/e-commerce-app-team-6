9d93d777a57cd98ec2efce20bf12684cbc718b95
Jyothyraj.Sobhana <jyothyraj.sobhana@global.com>
2023-06-01 10:33:20 +0100

    Added product to basket in back end

--- backend/src/main/java/org/global/ecp/hackathon/app/basket/BasketService.java
+++ backend/src/main/java/org/global/ecp/hackathon/app/basket/BasketService.java
@@ -27,9 +27,8 @@ public class BasketService {
     public Basket addToBasket(final Long productId) {
 
         BasketProduct basketProduct = basketProductService.createBasketProduct(getBasket(), productId);
-        basketRepository.add(basketProduct);
         basketProduct.increaseQuantity();
-        return null;
+        return basketRepository.add(basketProduct);;
     }
 
     // TODO - Task 5: remove basketProduct from the basket using the basketRepository
